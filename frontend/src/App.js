facb86f42684097ad08547d0ac244dc1bf09f09a
Mann-tech13 <mannshah.ict19@gmail.com>
2023-03-31 18:25:36 +0530

    Folder structure | Navbar | Sidebar

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './layout/Navbar/Navbar';
+import SidebarNav from './layout/SidebarNav/SidebarNav';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/HR/Dashboard/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div>HR</div>
+  )
+}
+
+export default Dashboard
