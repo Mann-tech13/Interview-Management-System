6ee52992c6c7dcb8196453a047d9895552830230
khushali452 <khushalivasani.ict19@gmail.com>
2023-04-06 17:25:29 +0530

    backend APIs

--- frontend/src/content/HR/CandidateContent.jsx
+++ frontend/src/content/HR/CandidateContent.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
+import axios from "axios";
 import "../../assets/CSS/HR/CandidateContent.css"
 import AddCandidate from '../Forms/AddCandidate';
 
 function CandidateContent() {
     const [formVisible, setFormVisible] = useState(false)
+    const [displayCandidate, setDisplayCandidate] = useState([])
 
     const header = (
         <div>
@@ -18,23 +20,44 @@ function CandidateContent() {
             <Button label="Edit" />
         </div>
     );
+
+    useEffect(() => {
+        axios.get(`http://localhost:8000/candidate`).then((response) => {
+            setDisplayCandidate(candidates => response.data)
+        })
+    }, [])
+
+
     return (
-        <div className="candidate-container">
-            <div className={"form-data "+formVisible}>
+        <div className="">
+            <div className={"form-data " + formVisible}>
                 <div className="icon-container">
                     <i className='plus-btn pi pi-plus' onClick={() => setFormVisible(true)}></i>
                 </div>
-                <div className="card flex justify-content-center card-container">
-                    <Card title="Title" subTitle="Subtitle" footer={footer} header={header} className="md:w-25rem">
-                        <p className="m-0 card-content">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae
-                            numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
-                        </p>
-                    </Card>
-                </div>
             </div>
-            <div className="">
-                {formVisible && <AddCandidate visible={formVisible} setVisible={setFormVisible} />}
+            <div className="candidate-container">
+
+                {
+                    displayCandidate.map((candidate) => {
+                        return (
+                            <div>
+                                <div className={"form-data " + formVisible}>
+                                    <div className="card flex justify-content-center card-container">
+                                        <Card title={candidate.firstName} subTitle={candidate.role} footer={footer} header={header} className="md:w-25rem">
+                                            <p className="m-0 card-content">
+                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae
+                                                numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+                                            </p>
+                                        </Card>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+                <div className="">
+                    {formVisible && <AddCandidate visible={formVisible} setVisible={setFormVisible} />}
+                </div>
             </div>
         </div>
     )
